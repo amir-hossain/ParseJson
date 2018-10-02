3679dc70d59496e30b643f1cf6fb35dfa9611bdc
amir <bladyamir@gmail.com>
2018-10-03 03:48:20 +0600

    start

---
+++ server.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var xls = require('simple-export-excel');
+const loadJsonFile = require('load-json-file');
+
+function readJsonFile() {
+    loadJsonFile('foo.json').then(json => {
+
+        createExcell(json);
+        //=> {foo: true}
+    });
+}
+
+function createExcell(data) {
+    console.log(data);
+    // var headers =
+    //     [
+    //         [
+    //             "ID", "Mood","Time","Activity"
+    //         ]
+    //     ]
+
+    var headers = 
+[
+    [
+        "item", "price"
+    ],
+    [
+        "name", "id"
+    ]
+]
+
+    var ret = xls.exportXls(headers, data);
+    console.log(data);
+    var fs = require('fs');
+    fs.writeFileSync('./test.xlsx', ret, 'binary')
+}
+
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plane' });
+    response.write('Hello world');
+    // createExcell();
+    readJsonFile();
+    response.end();
+}
+
+http.createServer(onRequest).listen(9000);
